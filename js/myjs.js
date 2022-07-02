275d00767911f31c4c6cbb409ad2dff3e70da6bd
hhhabcc <hhhabcc@outlook.com>
2022-07-02 15:50:16 +0800

    Site updated: 2022-07-02 15:50:16

---
+++ js/myjs.js
@@ -0,0 +1,7 @@
+// let backimg = ["url(/images/1.jpg)", 
+//   "url(/images/4.jpg)", 
+//   "url(/images/8.jpg)", 
+//   "url(/images/background.jpg)"
+// ]; 
+// let index = Math.floor(Math.random() * backimg.length); 
+// document.getElementById("web_bg").style.backgroundImage = backimg[index]
